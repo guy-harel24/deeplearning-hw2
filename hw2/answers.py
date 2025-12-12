ce54fa49a2ebce772432efb6da7a2011565d2417
eden-avidan <eden.avidan2000@gmail.com>
2025-12-12 16:53:32 +0200

    answered 2nd open question in part4

--- hw2/answers.py
+++ hw2/answers.py
@@ -228,8 +228,8 @@ def part4_optim_hp():
     # ====== YOUR CODE: ======
     loss_fn = torch.nn.CrossEntropyLoss()
     lr = 0.01
-    weight_decay = 1e-4
-    momentum = 0.9
+    weight_decay = 1e-4  # L2 Regularization to prevent overfitting
+    momentum = 0.9       # Accelerates SGD in the relevant direction
     # ========================
     return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
 
@@ -249,16 +249,46 @@ An equation: $e^{i\pi} -1 = 0$
 
 
 part4_q2 = r"""
-**Your answer:**
+**Question 1**
+
+First, let's calculate The Jacobian matrix $\frac{\partial y_1}{\partial x_1}$:
+    
+$$\forall i \le m, j \le n \quad \left[ \frac{\partial y_1}{\partial x_1} \right]_{i, j} = \frac{\partial y_{1, i}}{\partial x_{1, j}}$$
+    
+Using the definition above we get $y_{1, i} = \sum_{k=1}^{n} M_{i k} x_{1, k}$:
+    
+$$\frac{\partial y_{1, i}}{\partial x_{1, j}} = \frac{\partial}{\partial x_{1, j}} \left( \sum_{k=1}^{n} M_{i k} x_{1, k} \right) = \frac{\partial}{\partial x_{1, j}} (M_{i, j} x_{1, j}) = M_{i j} \Rightarrow \frac{\partial y_1}{\partial x_1} = M$$
+
+    
+We can now apply the chain rule:
+    
+$$\frac{\partial L}{\partial x_1} = \left(\frac{\partial y_1}{\partial x_1}\right)^T \cdot \frac{\partial L}{\partial y_1} \Rightarrow \frac{\partial L}{\partial x_1} = M^T \frac{\partial L}{\partial y_1}$$
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+**Question 2**
 
+We calculate the Jacobian $\frac{\partial y_2}{\partial x_2}$ using the sum rule for derivatives:
+
+$$\frac{\partial y_2}{\partial x_2} = \frac{\partial (x_2)}{\partial x_2} + \frac{\partial (M x_2)}{\partial x_2} = I + M$$
+
+Applying the chain rule:
+$$\frac{\partial L}{\partial x_2} = \left( \frac{\partial y_2}{\partial x_2} \right)^T \frac{\partial L}{\partial y_2} \Rightarrow \frac{\partial L}{\partial x_2} = (I + M)^T \frac{\partial L}{\partial y_2} \Rightarrow \frac{\partial L}{\partial x_2} = (I + M^T) \frac{\partial L}{\partial y_2}$$
+
+**Question 3**
+
+We assumed that $M$ has small entries, which is standard for weight initialization.
+
+When multiplying many matrices with small norms together, the product might approache zero exponentially fast.
+In the standard case, the gradient signal passes exclusively through the weights $M$. 
+
+Since $M$ is small, the gradient effect gets smaller at every single step.
+After dozens of layers, the "signal" is effectively (or literally) destroyed, and the early layers stop learning.
+
+In the residual case, the gradient is multiplied by $(I + M^T)$. This creates two "pathways" for the gradient:
+1. The Residual Path ($M^T$): The standard learned path, which might shrink the gradient.
+2. The Identity Path ($I$): The term $\frac{\partial L}{\partial y_2}$ is passed directly to the previous layer unchanged (multiplied by 1).
+
+Even if $M$ is very small (or even zero due to multiplications results and/or numerical errors), the gradient can flow backward through the Identity term without being diminished. This ensures that the gradient signal can reach the earliest layers of the network regardless of how deep the network is, effectively solving the vanishing gradient problem.
 """
 
 # ==============
